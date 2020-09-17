3c58b2e4169078f9c642c29758a694f1b74fe606
Oleg Tyunin <oleg.tyunin.dev@gmail.com>
2020-09-17 10:44:03 +0300

    button component

---
+++ src/components/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <button v-bind="$attrs">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button"
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  border: 2px solid black;
+  background-color: white;
+  font-size: 18px;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+
+  &:focus,
+  &:active {
+    outline: none;
+  }
+
+  &:hover {
+    background-color: black;
+    border-color: transparent;
+    color: white;
+  }
+}
+</style>
